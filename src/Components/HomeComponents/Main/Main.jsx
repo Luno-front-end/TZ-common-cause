84be03fd22c9eeab0ef61012c3c51d9119d59d4b
Luno-front-end <63469409+Luno-front-end@users.noreply.github.com>
2021-05-06 02:04:37 +0300

    add btn volume

--- src/Components/HomeComponents/Main/Main.jsx
+++ src/Components/HomeComponents/Main/Main.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import { FaVolumeUp } from "react-icons/fa";
+import classnames from "classnames";
+
 export default function Main() {
+  const [mutedValues, setMutedValues] = useState(true);
+  const [hideBtn, setРideBtn] = useState(false);
+
+  function mutedVolume() {
+    setMutedValues(false);
+    setРideBtn(true);
+  }
+
   return (
     <div className="player-container">
       <video
         className="player"
         autoPlay
-        muted
+        muted={mutedValues}
         controls
         width="620"
         height="370"
@@ -14,6 +26,14 @@ export default function Main() {
           type="video/mp4"
         />
       </video>
+      <button
+        onClick={mutedVolume}
+        className={classnames("btn-video-volume", { hideBtn })}
+        type="button"
+      >
+        <FaVolumeUp className="volume-icon" />
+        Включить звук
+      </button>
     </div>
   );
 }
