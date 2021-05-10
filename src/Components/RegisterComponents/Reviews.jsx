9e65233132f95f835e34b2b26177c86ba85cc0dd
Luno-front-end <63469409+Luno-front-end@users.noreply.github.com>
2021-05-11 00:37:15 +0300

    add section reviews

--- src/Components/RegisterComponents/Reviews.jsx
+++ src/Components/RegisterComponents/Reviews.jsx
@@ -6,24 +6,32 @@ export default function Reviews() {
     <section className="section-reviews">
       <h2 className="heading-one-reviews">Отлично! Вы справились!</h2>
       <div className="container-reviews">
-        <h3 className="heading-one-reviews">
+        <h3 className="heading-two-reviews">
           Добро пожаловать в «Общее дело».
         </h3>
 
         <article className="card-reviews">
-          <p>Реальные отзывы от пользователей программы «Общее дело»:</p>
+          <p className="real-reviews">
+            Реальные отзывы от пользователей программы «Общее дело»:
+          </p>
           <ul className="list-reviews">
             <li className="item-reviews">
-              <h4 className="heading-user-reviews">Михаил</h4>
-              <p className="info-reviews">только что заработал(а)</p>
-              <p className="earnings-reviews">$ 333.33</p>
               <img src={userOne} alt="user" className="img-user-reviews" />
+
+              <div className="containerinfo-card-reviews">
+                <h4 className="heading-user-reviews">Михаил</h4>
+                <p className="info-reviews">только что заработал(а)</p>
+                <p className="earnings-reviews">$ 333.33</p>
+              </div>
             </li>
             <li className="item-reviews">
-              <h4 className="heading-user-reviews">Руслан</h4>
-              <p className="info-reviews">только что заработал(а)</p>
-              <p className="earnings-reviews">$ 333.33</p>
               <img src={usertwo} alt="user" className="img-user-reviews" />
+
+              <div className="containerinfo-card-reviews">
+                <h4 className="heading-user-reviews">Руслан</h4>
+                <p className="info-reviews">только что заработал(а)</p>
+                <p className="earnings-reviews">$ 333.33</p>
+              </div>
             </li>
           </ul>
         </article>
